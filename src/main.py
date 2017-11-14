f04f73062791e00d3a4569cd79af19b39d8ad638
sidious1991 <agostinistefano1991@gmail.com>
2017-11-14 12:49:44 +0100

    got added

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 import tweepy
 import json
+import got
+
 from test.test_decimal import file
 
-#auth = tweepy.OAuthHandler(...)
-#auth.set_access_token(...)
-
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
 api = tweepy.API(auth)
-
+'''
 file_input = open("../tweets/democratic-candidate-timelines.txt", "r")
+
 id_list = []
 
 for line in file_input:
     id_list.append(int(line))
-    
+ 
 file_input.close()
 
 status_list = api.statuses_lookup(id_list[0:5])
@@ -25,9 +27,27 @@ for status in status_list:
 
 file_output.close()
 
-#print(status_list)
+print(status_list)
+'''
+'''
+tweets = tweepy.Cursor(api.search, q = "#finoallafine", since="2017-11-8", until="2017-11-10",).items()
 
+for tweet in tweets:
+    json_str = json.dumps(tweet._json) #json.dumps(json object) returns a string
+    print(json_str)
+'''  
 
+if __name__ == '__main__':  
+    tweetCriteria = got.manager.TweetCriteria().setSince("2017-11-1").setUntil("2017-11-10").setQuerySearch("#eleSicilia").setMaxTweets(5)
+    tweet = got.manager.TweetManager.getTweets(tweetCriteria)[3]
+    list = []  
+      
+    #for tweet in tweets:
+    list = api.retweets(tweet.id)
+        
+    print tweet.text
     
-    
-    
+    for status in list:
+        json_str = json.dumps(status._json) #json.dumps(json object) returns a string
+        print("retweet: ")
+        print(json_str)
